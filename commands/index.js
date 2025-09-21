6df1c9d85f0f7706dc453064aa28177aee6b01ae
khassamx <khasamxx@gmail.com>
2025-09-21 19:53:14 -0300

    Crear commands/index.js

---
+++ commands/index.js
@@ -0,0 +1,10 @@
+// commands/index.js
+module.exports = {
+    setsms: require('./setsms'),
+    subbot: require('./subbot'),
+    ytv: require('./ytv'),
+    playaudio: require('./playaudio'),
+    kick: require('./kick'),
+    antilink: require('./antilink'),
+    help: require('./help'),
+};
